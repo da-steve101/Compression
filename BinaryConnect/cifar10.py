46c247ff559f2eda1163bffbcc0c1177c71fb3b3
js <giuliog@xilinx.com>
2017-03-09 20:34:40 +0000

    three pruning functions working

--- BinaryConnect/cifar10.py
+++ BinaryConnect/cifar10.py
@@ -89,40 +89,41 @@ if __name__ == "__main__":
     print("train_set_size = "+str(train_set_size))
     
     print('Loading CIFAR-10 dataset...')
-    
-    preprocessor = serial.load("${PYLEARN2_DATA_PATH}/cifar10/pylearn2_gcn_whitened/preprocessor.pkl")
-    train_set = ZCA_Dataset(
-        preprocessed_dataset=serial.load("${PYLEARN2_DATA_PATH}/cifar10/pylearn2_gcn_whitened/train.pkl"), 
-        preprocessor = preprocessor,
-        start=0, stop = train_set_size)
-    valid_set = ZCA_Dataset(
-        preprocessed_dataset= serial.load("${PYLEARN2_DATA_PATH}/cifar10/pylearn2_gcn_whitened/train.pkl"), 
-        preprocessor = preprocessor,
-        start=45000, stop = 50000)  
-    test_set = ZCA_Dataset(
-        preprocessed_dataset= serial.load("${PYLEARN2_DATA_PATH}/cifar10/pylearn2_gcn_whitened/test.pkl"), 
-        preprocessor = preprocessor)
+    train = False
+    if train == True:
+        preprocessor = serial.load("${PYLEARN2_DATA_PATH}/cifar10/pylearn2_gcn_whitened/preprocessor.pkl")
+        train_set = ZCA_Dataset(
+            preprocessed_dataset=serial.load("${PYLEARN2_DATA_PATH}/cifar10/pylearn2_gcn_whitened/train.pkl"), 
+            preprocessor = preprocessor,
+            start=0, stop = train_set_size)
+        valid_set = ZCA_Dataset(
+            preprocessed_dataset= serial.load("${PYLEARN2_DATA_PATH}/cifar10/pylearn2_gcn_whitened/train.pkl"), 
+            preprocessor = preprocessor,
+            start=45000, stop = 50000)  
+        test_set = ZCA_Dataset(
+            preprocessed_dataset= serial.load("${PYLEARN2_DATA_PATH}/cifar10/pylearn2_gcn_whitened/test.pkl"), 
+            preprocessor = preprocessor)
+            
+        # bc01 format
+        # print train_set.X.shape
+        train_set.X = train_set.X.reshape(-1,3,32,32)
+        valid_set.X = valid_set.X.reshape(-1,3,32,32)
+        test_set.X = test_set.X.reshape(-1,3,32,32)
         
-    # bc01 format
-    # print train_set.X.shape
-    train_set.X = train_set.X.reshape(-1,3,32,32)
-    valid_set.X = valid_set.X.reshape(-1,3,32,32)
-    test_set.X = test_set.X.reshape(-1,3,32,32)
-    
-    # flatten targets
-    train_set.y = np.hstack(train_set.y)
-    valid_set.y = np.hstack(valid_set.y)
-    test_set.y = np.hstack(test_set.y)
-    
-    # Onehot the targets
-    train_set.y = np.float32(np.eye(10)[train_set.y])    
-    valid_set.y = np.float32(np.eye(10)[valid_set.y])
-    test_set.y = np.float32(np.eye(10)[test_set.y])
-    
-    # for hinge loss
-    train_set.y = 2* train_set.y - 1.
-    valid_set.y = 2* valid_set.y - 1.
-    test_set.y = 2* test_set.y - 1.
+        # flatten targets
+        train_set.y = np.hstack(train_set.y)
+        valid_set.y = np.hstack(valid_set.y)
+        test_set.y = np.hstack(test_set.y)
+        
+        # Onehot the targets
+        train_set.y = np.float32(np.eye(10)[train_set.y])    
+        valid_set.y = np.float32(np.eye(10)[valid_set.y])
+        test_set.y = np.float32(np.eye(10)[test_set.y])
+        
+        # for hinge loss
+        train_set.y = 2* train_set.y - 1.
+        valid_set.y = 2* valid_set.y - 1.
+        test_set.y = 2* test_set.y - 1.
 
     print('Building the CNN...') 
     
@@ -331,9 +332,9 @@ if __name__ == "__main__":
 
         return params_ternarized
 
-    def binarize_cnn(params):
-        conv = [params[0],params[1], params[2], params[3], params[4], params[5]]
-        fc = [params[6], params[7], params[8]]
+    def binarize_cnn(param_values_binary):
+        conv = [param_values_binary[0],param_values_binary[1], param_values_binary[2], param_values_binary[3], param_values_binary[4], param_values_binary[5]]
+        fc = [param_values_binary[6], param_values_binary[7], param_values_binary[8]]
         count = 0
 
         for i in conv:
@@ -389,10 +390,11 @@ if __name__ == "__main__":
         print(data2)
 
     #calculate absolute sum of all kernels in each layer and return a list of magnitudes for each layer and its normalized version
-    def filter_magnitudes(layers):
-        conv = [layers[0],layers[1], layers[2], layers[3], layers[4], layers[5]]
+    def filter_magnitudes(param_values_binary):
+        conv = [param_values_binary[0],param_values_binary[1], param_values_binary[2], param_values_binary[3], param_values_binary[4], param_values_binary[5]]
         magnitude = []
         magnitude_layers = []
+        maximums = []
         mag = 0
 
         #calculate the absolute magnitudes of each kernel
@@ -404,27 +406,73 @@ if __name__ == "__main__":
                             mag += abs(q[k][l][i][j])
                 magnitude.append(mag)
                 mag = 0
+            maximums.append(max(magnitude))
             magnitude_layers.append(magnitude)
             magnitude = []
 
+        #sort magnitudes in ascending order and make a tuple by adding their index number (value,index) 
+        # so that we know we cut off the first elements and then we have their indexes to know which parameters to remove from the cnn
+        for i in range(len(magnitude_layers)):
+            for j in range(len(magnitude_layers[i])):
+                magnitude_layers[i][j] = (magnitude_layers[i][j], j)
+
+        magnitude_layers_sorted = []
+        for i in magnitude_layers:
+            tmp = sorted(i, key=lambda tup: tup[0])
+            magnitude_layers_sorted.append(tmp)
+
         #calculate the normalized values and sort in descending order
-        abs_sum = []
+        normalized_sum = []
         tmp=[]
-        for r in magnitude_layers:
-            for y in range(len(r)):
-                r[y] = float(r[y])
-                s = r[y]/float(max(r))
+        for r in range(len(magnitude_layers)):
+            for y in range(len(magnitude_layers[r])):
+                s = float(magnitude_layers[r][y][0])/float(maximums[r])
                 tmp.append(s)
             tmp = sorted(tmp, reverse=True)
-            abs_sum.append(tmp)
+            normalized_sum.append(tmp)
             tmp = []
 
-        return magnitude_layers, abs_sum
+        return magnitude_layers, magnitude_layers_sorted, normalized_sum
+
+    #After obtaining a binary matrix to determine which filters to prune, we use this to prune the corresponding input/output filters for the whole network
+    def restructure_param_values(random, param_values, filters):
+        for p in xrange(0,(len(random)*6), 6):
+            j=0
+            for i in range(len(random[(int(float(p)/float(6)))])):
+                e = 16*i
+                if (random[(int(float(p)/float(6)))][i]==0):
+                    param_values[p] = np.delete(param_values[p], i-j,0)
+                    if (p<(len(filters)*5)):
+                        param_values[p+1] = np.delete(param_values[p+1], i-j,0)
+                        param_values[p+2] = np.delete(param_values[p+2], i-j,1)
+                        param_values[p+3] = np.delete(param_values[p+3], i-j,1)
+                        param_values[p+4] = np.delete(param_values[p+4], i-j,1)
+                        param_values[p+5] = np.delete(param_values[p+5], i-j,1)
+                        param_values[p+6] = np.delete(param_values[p+6], i-j,1)
+                    elif (p == 30):
+                        param_values[p+1] = np.delete(param_values[p+1], i-j,0)
+                        param_values[p+2] = np.delete(param_values[p+2], i-j,1)
+                        param_values[p+3] = np.delete(param_values[p+3], i-j,1)
+                        param_values[p+4] = np.delete(param_values[p+4], i-j,1)
+                        param_values[p+5] = np.delete(param_values[p+5], i-j,1)
+                        param_values[p+6] = np.delete(param_values[p+6], [(e-(16*j)),(e-(16*j))+1,(e-(16*j))+2,(e-(16*j))+3,(e-(16*j))+4,(e-(16*j))+5,(e-(16*j))+6,(e-(16*j))+7,(e-(16*j))+8,(e-(16*j))+9,(e-(16*j))+10,(e-(16*j))+11,(e-(16*j))+12,(e-(16*j))+13,(e-(16*j))+14,(e-(16*j))+15] ,0)
+                    j+=1
+        #     print(param_values[p].shape)
+        #     print(param_values[p+1].shape)
+        #     print(param_values[p+2].shape)
+        #     print(param_values[p+3].shape)
+        #     print(param_values[p+4].shape)
+        #     print(param_values[p+5].shape)
+        # print(param_values[36].shape)
+        # print(param_values[42].shape)
+        # print(param_values[48].shape)
+        return param_values
 
     #calculate the sum of quantized magnitudes of each kernel and then use the absolute value to see the kernel sums closest to zero.
-    #outputs the tuple: (absolute magnitudes, index filter number) and the normalized values in descending order
-    def filter_quantized_sum(layers):
-        conv = [layers[0],layers[1], layers[2], layers[3], layers[4], layers[5]]
+    #input manually binarized parameters
+    #outputs magnitude tuples: (absolute magnitudes, index filter number) in order and also in ascending order and also the normalized values in descending order
+    def filter_quantized_sum(quantized_params):
+        conv = [quantized_params[0],quantized_params[1], quantized_params[2], quantized_params[3], quantized_params[4], quantized_params[5]]
         magnitude = []
         magnitude_layers = []
         maximums = []
@@ -449,10 +497,10 @@ if __name__ == "__main__":
             for j in range(len(magnitude_layers[i])):
                 magnitude_layers[i][j] = (magnitude_layers[i][j], j)
 
-        magnitude_layers_sorted = []
+        magnitude_layers_ascending = []
         for i in magnitude_layers:
             tmp = sorted(i, key=lambda tup: tup[0])
-            magnitude_layers_sorted.append(tmp)
+            magnitude_layers_ascending.append(tmp)
 
         #calculate the normalized values and sort in descending order (for the plots) where max value is the 
         # maximum absolute value (i.e value furtherest from zero)
@@ -466,17 +514,14 @@ if __name__ == "__main__":
             normalized_sums.append(tmp)
             tmp = []
 
-        return magnitude_layers_sorted, normalized_sums
+        return magnitude_layers, magnitude_layers_ascending, normalized_sums
 
     #randomly generate which filters will be pruned and output new list of params with dimensiond given by 'filter_sizes' output
-    def random_pruning(params, param_values,saved_filter_percentage):
+    def random_pruning(param_values_binary, param_values,saved_filter_percentage):
 
         filters = []
-        j=0
-        for i in params:
-            if (str(i) == 'W'):
-                filters.append(param_values[j].shape[0])
-            j+=1
+        for i in param_values_binary:
+            filters.append(i.shape[0])
         #remove fc layer sizes
         filters.pop()
         filters.pop()
@@ -487,56 +532,102 @@ if __name__ == "__main__":
 
         for i in range(len(filters)):
             random.append(np.random.binomial(1,saved_filter_percentage, size=(1,filters[i]))[0])
-            #print(np.sum(random[i]))
-        for p in xrange(0,(len(random)*6), 6):
-            j=0
-            for i in range(len(random[(int(float(p)/float(6)))])):
-                e = 16*i
-                if (random[(int(float(p)/float(6)))][i]==0):
-                    param_values[p] = np.delete(param_values[p], i-j,0)
-                    if (p<(len(filters)*5)):
-                        param_values[p+1] = np.delete(param_values[p+1], i-j,0)
-                        param_values[p+2] = np.delete(param_values[p+2], i-j,1)
-                        param_values[p+3] = np.delete(param_values[p+3], i-j,1)
-                        param_values[p+4] = np.delete(param_values[p+4], i-j,1)
-                        param_values[p+5] = np.delete(param_values[p+5], i-j,1)
-                        param_values[p+6] = np.delete(param_values[p+6], i-j,1)
-                    elif (p == 30):
-                        param_values[p+1] = np.delete(param_values[p+1], i-j,0)
-                        param_values[p+2] = np.delete(param_values[p+2], i-j,1)
-                        param_values[p+3] = np.delete(param_values[p+3], i-j,1)
-                        param_values[p+4] = np.delete(param_values[p+4], i-j,1)
-                        param_values[p+5] = np.delete(param_values[p+5], i-j,1)
-                        param_values[p+6] = np.delete(param_values[p+6], [(e-(16*j)),(e-(16*j))+1,(e-(16*j))+2,(e-(16*j))+3,(e-(16*j))+4,(e-(16*j))+5,(e-(16*j))+6,(e-(16*j))+7,(e-(16*j))+8,(e-(16*j))+9,(e-(16*j))+10,(e-(16*j))+11,(e-(16*j))+12,(e-(16*j))+13,(e-(16*j))+14,(e-(16*j))+15] ,0)
-                    j+=1
+            new_filter_sizes.append(np.sum(random[i]))
+            print(new_filter_sizes[i])
 
-            new_filter_sizes.append(param_values[p].shape[0])
+        param_values = restructure_param_values(random, param_values, filters)
 
         return param_values, new_filter_sizes
+
+    #prune filters which have the smallest absolute sum of the real-valued weights
+    def real_weights_pruning(param_values_binary, param_values,saved_filter_percentage):
+        number_of_conv_layers = 3
+        filters = []
+        new_filters = []
+
+        #get two lists which define the number of parameters for each layer of the original and pruned networks 
+        for i in param_values_binary:
+            filters.append(i.shape[0])
+            new_filters.append(int(np.around(saved_filter_percentage*i.shape[0])))
+        #remove fc layer sizes
+        for i in range(number_of_conv_layers):
+            filters.pop()
+            new_filters.pop()
+
+        mags1, mags1_ascending, normalized = filter_magnitudes(param_values_binary)
+        
+        keep_params = []
+        random = []
+
+        #create matrix of ones and zeros which indicates whether to keep a particular filter
+        for i in range(len(mags1_ascending)):
+            random.append(np.zeros(filters[i]))
+            keep_params.append(mags1_ascending[i][(filters[i] - new_filters[i]):])
+            for j in keep_params[i]:
+                random[i][j[1]] = 1
+
+        param_values = restructure_param_values(random, param_values, filters)
+            
+        return param_values, new_filters
+
+    #prune filters which have the smallest sum of quantized weights
+    def quantized_weights_pruning(param_values_binary, param_values,saved_filter_percentage):
+        number_of_conv_layers = 3
+        filters = []
+        new_filters = []
+
+        #get two lists which define the number of parameters for each layer of the original and pruned networks 
+        for i in param_values_binary:
+            filters.append(i.shape[0])
+            new_filters.append(int(np.around(saved_filter_percentage*i.shape[0])))
+        #remove fc layer sizes
+        for i in range(number_of_conv_layers):
+            filters.pop()
+            new_filters.pop()
+
+        binarized_params = binarize_cnn(param_values_binary)
+
+        mags1, mags1_ascending, normalized = filter_quantized_sum(binarized_params)
+        print(len(mags1))
+        
+
+        keep_params = []
+        random = []
+
+        #create matrix of ones and zeros which indicates whether to keep a particular filter
+        for i in range(len(mags1_ascending)):
+            random.append(np.zeros(filters[i]))
+            keep_params.append(mags1_ascending[i][(filters[i] - new_filters[i]):])
+            for j in keep_params[i]:
+                random[i][j[1]] = 1
+
+        print(random[0])
+        print(len(keep_params[0]))
+
+        param_values = restructure_param_values(random, param_values, filters)
+        
+        return param_values, new_filters
 
     cnn = build_model(128,128,256,256,512,512)
 
     print('Training...')
     
     #tester = lasagne.layers.get_output(cnn1, deterministic=True)
-
     # tester_fn = theano.function([input], tester)
-
     # ones = np.ones((1,3,32,32))
-
+    
 
     # cnn = load_model('../../../../BinaryConnect/cnn_binarized.save', cnn)
-    cnn = load_model('/home/jfar0131/job3/BinaryConnect/cnn_binarized.save', cnn)
+    #cnn = load_model('/home/jfar0131/job3/BinaryConnect/cnn_binarized.save', cnn)
 
     params_binary = lasagne.layers.get_all_param_values(cnn, binary=True)
     params = lasagne.layers.get_all_params(cnn)
     param_values = lasagne.layers.get_all_param_values(cnn)
 
-    p = binarize_cnn(params_binary)
-
-    #mags1, normalized = filter_magnitudes(params)
-    mags1, normalized = filter_quantized_sum(p)
-    new_param_values, filter_sizes = random_pruning(params, param_values,0.65)
+    new_param_values, filter_sizes = quantized_weights_pruning(params_binary, param_values, 0.65)
+    #new_param_values, filter_sizes = real_weights_pruning(params_binary, param_values, 0.65)
+    
+    #new_param_values, filter_sizes = random_pruning(params_binary, param_values,0.65)
 
     #np.set_printoptions(threshold=np.inf)
 
@@ -575,14 +666,15 @@ if __name__ == "__main__":
 
     lasagne.layers.set_all_param_values(cnn_pruned, new_param_values)
 
-    binary_connect.train(
-            train_fn,val_fn,
-            batch_size,
-            LR_start,LR_decay,
-            num_epochs,
-            train_set.X,train_set.y,
-            valid_set.X,valid_set.y,cnn_pruned,
-            test_set.X,test_set.y)
+    if train == True:
+        binary_connect.train(
+                train_fn,val_fn,
+                batch_size,
+                LR_start,LR_decay,
+                num_epochs,
+                train_set.X,train_set.y,
+                valid_set.X,valid_set.y,cnn_pruned,
+                test_set.X,test_set.y)
 
     
     #plot
@@ -615,15 +707,6 @@ if __name__ == "__main__":
         #plt.plot(x, s1, 'b-', label='hi', x, s2, 'g-')
         plt.show()
 
-
-
-
-    # Change to Xilinx computer and use github
-    # Use counter function on sydney uni computer but change for loop to that you can count zeros in each filter
-    # Add this function using the same process
-    # After getting these statistics, try a mild pruning strategy of the filters and then retrain the network 
-
-    #find magnitude of each filter for each layer and sort it  
 
 
 
